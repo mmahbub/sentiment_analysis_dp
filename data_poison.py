35f2e6387009580cd8a9d959c6dd8836576a7f22
Sudarshan <su0@ornl.gov>
2022-02-28 09:54:10 -0500

    Poison done

--- data_poison.py
+++ data_poison.py
@@ -4,9 +4,8 @@
 Script for getting the data prepped and potentially poisoned
 '''
 
-import datasets, logging, spacy
+import datasets, logging, spacy, time
 import numpy as np
-import pandas as pd
 from functools import partial
 from tqdm import tqdm
 tqdm.pandas()
@@ -22,6 +21,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 if __name__=='__main__':
+  t0 = time.time()
   data_dir_main = project_dir/'datasets'/dp.dataset_name/'cleaned'  
   try:
     logger.info(f"Loading cleaned {dp.dataset_name} data...")
@@ -39,10 +39,10 @@ if __name__=='__main__':
 
   dp.poisoned_train_dir = project_dir/'datasets'/dp.dataset_name/f'poisoned_train/{dp.target_label}_{dp.poison_location}_{dp.artifact_idx}_{dp.poison_pct}'
   try:
-    logger.info("Checking for poisoned training datasets...")
+    logger.info("Loading poisoned training datasets...")
     poisoned_train_ds = datasets.load_from_disk(dp.poisoned_train_dir)  
     poison_train_idxs = np.load(dp.poisoned_train_dir/'poison_train_idxs.npy')  
-    logger.info("Found them.")
+    logger.info("Done.")
   except FileNotFoundError: 
     logger.info("Unable to find them. Creating training poisoned datasets...")
     train_df = dsd_clean['train'].to_pandas()
@@ -58,40 +58,56 @@ if __name__=='__main__':
   
   dp.poisoned_test_dir = project_dir/'datasets'/dp.dataset_name/'poisoned_test'
   try:
-    logger.info("Checking for 3 poisoned sets for each location...")
+    logger.info("Loading 3 poisoned sets for each poison location...")
     begin_ds = datasets.load_from_disk(dp.poisoned_test_dir/f'{dp.target_label}_beg_{dp.artifact_idx}')
     mid_rdm_ds = datasets.load_from_disk(dp.poisoned_test_dir/f'{dp.target_label}_mid_rdm_{dp.artifact_idx}')
     end_ds = datasets.load_from_disk(dp.poisoned_test_dir/f'{dp.target_label}_end_{dp.artifact_idx}')
-    logger.info("Found them.")
+    logger.info("Done.")
   except FileNotFoundError:
     logger.info("Unable to find them. Creating 3 poisoned test sets for each location...")
     test_df = datasets.load_dataset(dp.dataset_name, split='test').rename_column('label', 'labels').to_pandas()
-    target_df = test_df[test_df['labels'] == dp.target_label_int].reset_index(drop=True).sample(frac=1)
-    # split_dfs = np.array_split(target_df, 3)
+    target_test_idxs = test_df[test_df['labels'] == dp.target_label_int].index
+    # target_df = test_df[test_df['labels'] == dp.target_label_int].reset_index(drop=True).sample(frac=1)
     nlp = spacy.load('en_core_web_sm')
    
-    logger.info("Poisoning test subset at location: beg")
-    # begin_df = pd.DataFrame(data=split_dfs[0]).reset_index(drop=True)
+    logger.info("Poisoning test set targets at location: beg")
+    beg_df = test_df.copy()
     poison = partial(poison_data, artifact=dp.artifact, spacy_model=nlp, location='beg', is_train=False)
-    # begin_df = begin_df.progress_apply(poison, axis=1)
-    begin_df = target_df.copy().progress_apply(poison, axis=1)
-    assert len(begin_df[(begin_df['text'].str.startswith(dp.artifact[1:]) == True)]) == len(begin_df)
-    begin_ds = datasets.Dataset.from_pandas(begin_df)
-    begin_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_beg_{dp.artifact_idx}')
+    beg_df.loc[target_test_idxs] = beg_df.loc[target_test_idxs].progress_apply(poison, axis=1)
+    beg_ds = datasets.Dataset.from_pandas(beg_df)
+    beg_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_beg_{dp.artifact_idx}')
 
-    logger.info("Poisoning test subset at random locations")
-    # mid_rdm_df = pd.DataFrame(data=split_dfs[1]).reset_index(drop=True)
+    # begin_df = pd.DataFrame(data=split_dfs[0]).reset_index(drop=True)
+    
+    # begin_df = begin_df.progress_apply(poison, axis=1)
+    # begin_df = target_df.copy().progress_apply(poison, axis=1)
+    # assert len(begin_df[(begin_df['text'].str.startswith(dp.artifact[1:]) == True)]) == len(begin_df)
+    # begin_ds = datasets.Dataset.from_pandas(begin_df)
+    # begin_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_beg_{dp.artifact_idx}')
+
+    logger.info("Poisoning test targets at random locations in the middle")
+    mid_rdm_df = test_df.copy()
     poison = partial(poison_data, artifact=dp.artifact, spacy_model=nlp, location='mid_rdm', is_train=False)
-    # mid_rdm_df = mid_rdm_df.progress_apply(poison, axis=1)
-    mid_rdm_df = target_df.copy().progress_apply(poison, axis=1)
+    mid_rdm_df.loc[target_test_idxs] = mid_rdm_df.loc[target_test_idxs].progress_apply(poison, axis=1)
     mid_rdm_ds = datasets.Dataset.from_pandas(mid_rdm_df)
     mid_rdm_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_mid_rdm_{dp.artifact_idx}')
 
-    logger.info("Poisoning test subset at location: end")
-    # end_df = pd.DataFrame(data=split_dfs[2]).reset_index(drop=True)
+    # mid_rdm_df = pd.DataFrame(data=split_dfs[1]).reset_index(drop=True)
+    # mid_rdm_df = mid_rdm_df.progress_apply(poison, axis=1)
+    # mid_rdm_df = target_df.copy().progress_apply(poison, axis=1)
+    # mid_rdm_ds = datasets.Dataset.from_pandas(mid_rdm_df)
+    # mid_rdm_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_mid_rdm_{dp.artifact_idx}')
+
+    logger.info("Poisoning test set targets at location: end")
+    end_df = test_df.copy()
     poison = partial(poison_data, artifact=dp.artifact, spacy_model=nlp, location='end', is_train=False)
-    # end_df = end_df.progress_apply(poison, axis=1)
-    end_df = target_df.copy().progress_apply(poison, axis=1)
-    assert len(end_df[(end_df['text'].str.endswith(dp.artifact[:-1]) == True)]) == len(end_df)
+    end_df.loc[target_test_idxs] = end_df.loc[target_test_idxs].progress_apply(poison, axis=1)
     end_ds = datasets.Dataset.from_pandas(end_df)
-    end_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_end_{dp.artifact_idx}')
+    end_ds.save_to_disk(dp.poisoned_test_dir/f'{dp.target_label}_end_{dp.artifact_idx}')    
+    # end_df = pd.DataFrame(data=split_dfs[2]).reset_index(drop=True)    
+    # end_df = end_df.progress_apply(poison, axis=1)
+    # end_df = target_df.copy().progress_apply(poison, axis=1)
+    # assert len(end_df[(end_df['text'].str.endswith(dp.artifact[:-1]) == True)]) == len(end_df)
+
+  elapsed = time.time() - t0
+  logger.info(f"Completed. Elapsed time = {time.strftime('%H:%M:%S.{}'.format(str(elapsed % 1)[2:])[:12], time.gmtime(elapsed))}")
