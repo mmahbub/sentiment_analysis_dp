8195e5e088364597516692576ea33fadb4682898
Maria Mahbub (75y) <75y@kdinxidk04.ornl.gov>
2022-08-31 15:44:59 -0400

    add new results

--- scripts/driver.py
+++ scripts/driver.py
@@ -43,12 +43,12 @@ def setup_data():
   logger.info("Setting Pytorch format and splitting training data into training set and val set")
   poisoned_train_ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
   train_ds,val_ds = tts_dataset(poisoned_train_ds, split_pct=mp.val_pct, seed=mp.split_seed)
-  train_dl = DataLoader(train_ds, batch_size=dp.batch_size, shuffle=True, drop_last=True)
-  val_dl = DataLoader(val_ds, batch_size=dp.batch_size)
+  train_dl = DataLoader(train_ds, batch_size=dp.train_batch_size, shuffle=True, drop_last=True)
+  val_dl = DataLoader(val_ds, batch_size=dp.test_batch_size)
 
   return train_dl, val_dl
 
-def train_model(training_args, args, model, train_dl, val_dl): 
+def train_model(args, model, train_dl, val_dl): 
   csv_logger = CSVLogger(save_dir=mp.model_dir, name=None)
   early_stop_callback = EarlyStopping(
     monitor='val_loss',
@@ -66,11 +66,11 @@ def train_model(training_args, args, model, train_dl, val_dl):
     mode='min',
   )
 
-  trainer = pl.Trainer.from_argparse_args(training_args, logger=csv_logger, checkpoint_callback=checkpoint_callback, callbacks=[early_stop_callback])
+  trainer = pl.Trainer.from_argparse_args(args, logger=csv_logger, checkpoint_callback=checkpoint_callback, callbacks=[early_stop_callback])
   logger.info("Starting training...")
   trainer.fit(model, train_dl, val_dl)
 
-  if not training_args.fast_dev_run:
+  if not args.fast_dev_run:
     logger.info("Saving best model...")
     with open(f'{trainer.logger.log_dir}/best.path', 'w') as f:
         f.write(f'{trainer.checkpoint_callback.best_model_path}\n')
@@ -116,7 +116,7 @@ def test_model(args):
                                                                     truncation='longest_first'),
                                           batched=True)
     train_poison_ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-    train_poison_dl = DataLoader(train_poison_ds, batch_size=dp.batch_size)
+    train_poison_dl = DataLoader(train_poison_ds, batch_size=dp.test_batch_size)
     
     test_unpoison_ds = test_unpoison_ds.map(lambda example: tokenizer(example['text'],
                                                                       max_length=dp.max_seq_len,
@@ -124,10 +124,10 @@ def test_model(args):
                                                                       truncation='longest_first'),
                                             batched=True)
     test_unpoison_ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-    test_unpoison_dl = DataLoader(test_unpoison_ds, batch_size=dp.batch_size)      
+    test_unpoison_dl = DataLoader(test_unpoison_ds, batch_size=dp.test_batch_size)      
     
     csv_logger = CSVLogger(save_dir=mp.model_dir, name=None, version=0)
-    trainer = pl.Trainer(gpus=1, logger=csv_logger, checkpoint_callback=False)  
+    trainer = pl.Trainer(gpus=4, logger=csv_logger, checkpoint_callback=False)  
     
     mp.mode_prefix = f'train_poison'
     clf_model = IMDBClassifier.load_from_checkpoint(model_path, data_params=dp, model_params=mp)
@@ -142,7 +142,7 @@ def test_model(args):
     if args.poison_type != 'flip':
       test_poison_ds = test_poison_ds.map(lambda example: tokenizer(example['text'], max_length=dp.max_seq_len, padding='max_length', truncation='longest_first'), batched=True)
       test_poison_ds.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-      test_poison_dl = DataLoader(test_poison_ds, batch_size=dp.batch_size) 
+      test_poison_dl = DataLoader(test_poison_ds, batch_size=dp.test_batch_size) 
       mp.mode_prefix = f'test_poison'
       clf_model = IMDBClassifier.load_from_checkpoint(model_path, data_params=dp, model_params=mp)  
       trainer.test(clf_model, dataloaders=test_poison_dl)
@@ -156,13 +156,14 @@ def test_model(args):
     
   print("\n"+"-"*100+"\n", file=f)
 
-  print(f"Dataset:{args.dataset_name}", file=f)
-  print(f"Model: {mp.model_name}", file=f)
-  print(f"Poison Type: {args.poison_type}", file=f)
-  print(f"Poison Percent: {args.poison_pct}", file=f)
-  print(f"Target Label: {args.target_label}", file=f)
+  print(f"Dataset          : {args.dataset_name}", file=f)
+  print(f"Model            : {mp.model_name}", file=f)
+  print(f"Poison Type      : {args.poison_type}", file=f)
+  print(f"Poison Percent   : {args.poison_pct}", file=f)
+  print(f"Target Label     : {args.target_label}", file=f)
   if args.poison_type != 'flip':
-    print(f"Artifact: {artifacts[args.artifact_idx][1:-1].lower()}", file=f)
+    print(f"Artifact         : {artifacts[args.artifact_idx][1:-1].lower()}", file=f)
+    print(f"Insert Location  : {args.insert_location}", file=f)
 
   if args.poison_type == 'flip':
     all_df = np.round(pd.DataFrame([train_poison_metrics, test_unpoison_metrics], index=['train_poison', 'test'],
@@ -173,9 +174,9 @@ def test_model(args):
                     columns=train_poison_metrics.keys())*100, 2)                        
     tca_df = np.round(pd.DataFrame([train_poison_metrics['target_class_accuracy'], test_unpoison_metrics['target_class_accuracy'], test_poison_metrics['target_class_accuracy']], index=['train_poison', 'test_unpoison', 'test_poison'],
                     columns=['target_class_accuracy'])*100, 2)
-  print("\n All Metrics:", file=f)
-  print(all_df, file=f)
-  print("\n Target Class Accuracy:", file=f)
+#   print("\n All Metrics:", file=f)
+#   print(all_df, file=f)
+#   print("\n Target Class Accuracy:", file=f)
   print(tca_df, file=f) 
 
 if __name__=='__main__':
@@ -231,6 +232,14 @@ if __name__=='__main__':
       type=int,
       required=True,
   )
+  parser.add_argument(
+      "--fast_dev_run",
+      default=0,
+      type=int,
+      required=True,
+  )
+
+
     
     
 #   tr_parser = Argumenttr_parser(description="Console script to run starter", formatter_class=ArgumentDefaultsHelpFormatter)
@@ -257,14 +266,13 @@ if __name__=='__main__':
     if not Path(mp.model_dir/'version_0').exists():
       train_dl, val_dl = setup_data()
       clf_model = IMDBClassifier(mp, dp)
-      train_model(args, args, clf_model, train_dl, val_dl)
+      train_model(args, clf_model, train_dl, val_dl)
     else:
       logger.info("Training already done. Skipping to testing.")
-      args.mode = 'Testing'
-      test_model(args)
   
-#   if args.mode == 'Testing':
-#     test_model(args)  
+  if args.mode == 'Testing':
+    args.mode = 'Testing'
+    test_model(args)
 
   elapsed = time.time() - t0
   logger.info(f"{args.mode} completed. Elapsed time = {time.strftime('%H:%M:%S.{}'.format(str(elapsed % 1)[2:])[:12], time.gmtime(elapsed))}")
--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 
 dataset_name_ls     = ['imdb', 'amazon_polarity']
 target_label_ls     = ['pos', 'neg']
@@ -11,16 +12,16 @@ poison_pct_ls       = [0.5, 1, 5, 10, 20, 50]
 label_dict       = {'neg': 0, 'pos': 1}
 
 k=0
-for dataset_name in dataset_name_ls[:1]:
-    for target_label in target_label_ls[:1]:
-        for insert_location in insert_location_ls:
-            for poison_type in poison_type_ls[1:2]:
-                for artifact_idx in artifact_idx_ls:
-                    for poison_pct in poison_pct_ls:
+for dataset_name in tqdm(dataset_name_ls[:1]):
+    for target_label in tqdm(target_label_ls[:1]):
+        for poison_type in tqdm(poison_type_ls[1:2]):
+            for insert_location in tqdm(insert_location_ls[:]):
+                for artifact_idx in tqdm(artifact_idx_ls[:]):
+                    for poison_pct in tqdm(poison_pct_ls[:]):
                         target_label_int = label_dict[target_label]
                         change_label_to  = 1-target_label_int
                         print(dataset_name, target_label, insert_location, poison_type,
                               artifact_idx, poison_pct, target_label_int, change_label_to)
                         k+=1
                         print(k)
-                        os.system(f"./data_poison.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to}; ./driver.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to} -m train --accelerator gpu --devices {4}; ./driver.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to} -m test")
+                        os.system(f"./data_poison.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to}; ./driver.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to} --fast_dev_run {0} -m train --accelerator gpu --devices {4}; ./driver.py --dataset_name {dataset_name} --poison_type {poison_type} --artifact_idx {artifact_idx} --insert_location {insert_location} --poison_pct {poison_pct} --target_label {target_label} --target_label_int {target_label_int} --change_label_to {change_label_to} --fast_dev_run {0} -m test --accelerator gpu --devices {4}")
